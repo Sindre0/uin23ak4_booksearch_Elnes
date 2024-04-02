1d79b37d7f12e250d5b60a4ca5683b3eb7d6e136
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-04-02 15:56:22 +0200

    lagt til et inputfelt

--- booksearch/src/components/SearchField.jsx
+++ booksearch/src/components/SearchField.jsx
@@ -3,7 +3,11 @@ export default function SearchField(){
     return(
         <>
         <h2>Søk etter bøker</h2>
-        <form></form>
+        <form>
+            <label htmlFor="search"></label>
+            <input type="text" id="search" placeholder="James Bond..."></input>
+            <input type="submit" value="Søk"></input>
+        </form>
         </>
     )
 }
