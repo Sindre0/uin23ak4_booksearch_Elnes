3796b6ac86e6dfc775eabe6d0c779d5d6b12c2e7
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-04-02 18:29:40 +0200

    lagt in usestates og api-kall
    har nå tilgang til informasjon fra apiet som deretter blir lagt i usestatene

--- booksearch/src/App.jsx
+++ booksearch/src/App.jsx
@@ -1,12 +1,36 @@
-
 import './App.css'
 import Layout from './components/Layout'
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [content, setContent] = useState()
+  const [query, setQuery] = useState("James Bond")
   
+  const getData = async()=>{
+    try {
+      const response = await fetch(`https://openlibrary.org/search.json?title=${query}`);
+      const data = await response.json();
+      setContent(data);
+
+    } catch {
+      console.error("En feil har oppstått.")
+    } 
+   }
+
+   useEffect(() => {
+    getData();
+  }, [query]);
+
+  console.log(content)
+
   return (
     <>
-     <Layout></Layout>
+     <Layout>
+      
+     </Layout>
     </>
   )
 }
